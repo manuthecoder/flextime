57a20e24c3569b7e7a2eb054217ec824e89e06cb
manuthecoder <manusvathgurudath@gmail.com>
2022-10-25 06:33:29 +0000

    update 5 files

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -63,10 +63,7 @@ function ProfileMenu() {
               e.preventDefault();
               // Stash the event so it can be triggered later.
               deferredPrompt = e;
-              // Update UI notify the user they can install the PWA
-              showInstallPromotion();
-              // Optionally, send analytics event that PWA install promo was shown.
-              console.log(`'beforeinstallprompt' event was fired.`);
+              deferredPrompt.prompt();
             });
             handleClose();
           }}
