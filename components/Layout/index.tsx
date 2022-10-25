637741f8c9d7d4a2cf636c8ff6b5d21ee804c277
manuthecoder <manusvathgurudath@gmail.com>
2022-10-25 18:11:50 +0000

    update 5 files

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -6,8 +6,37 @@ import { green } from "@mui/material/colors";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Toolbar from "@mui/material/Toolbar";
-import { signIn,  signOut, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
+
+function stringToColor(string: string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name: string) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+  };
+}
 
 function ProfileMenu({ session }) {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -87,6 +116,7 @@ function ProfileMenu({ session }) {
       </Menu>
 
       <Avatar
+        {...stringAvatar(session.user.name)}
         onClick={handleClick}
         sx={{
           cursor: "pointer",
@@ -94,6 +124,9 @@ function ProfileMenu({ session }) {
           height: 35,
           borderRadius: 3,
           background: green[200],
+          fontSize: "15px",
+          fontWeight: "100",
+          color: green[900],
         }}
       />
     </Box>
