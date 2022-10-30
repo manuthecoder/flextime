37ede969f58153543a48e305eaa4204554fe184f
manuthecoder <manusvathgurudath@gmail.com>
2022-10-30 18:57:05 +0000

    update create.ts

--- pages/api/appointments/create.ts
+++ pages/api/appointments/create.ts
@@ -8,21 +8,44 @@ export default async function handle(
 ) {
   //  Create a new appointment
   const { studentId, teacherCreated, flexId, date, reason }: any = req.body;
-  const result = await prisma.appointment.create({
-    data: {
-      reason: reason ?? "",
-      teacherCreated: teacherCreated === "true",
-      student: {
-        connect: {
-          studentId,
-        },
-      },
-      date: req.body.date,
+  // Upsert an apppointment based on identifier
+  const identifier = `${studentId},${date}`;
+
+  const appointment = await prisma.appointment.upsert({
+    where: {
+      identifier: identifier,
+    },
+    update: {
       flexChoice: {
         connect: {
-          id: parseInt(flexId.toString()),
+          id: flexId,
         },
       },
+      teacherCreated: teacherCreated === "true",
+      reason: reason,
+      student: {
+        connect: {
+          id: studentId,
+        },
+      },
+      date: date,
+      attended: false,
+    },
+    create: {
+      flexChoice: {
+        connect: {
+          id: flexId,
+        },
+      },
+      teacherCreated: teacherCreated === "true",
+      reason: reason,
+      student: {
+        connect: {
+          id: studentId,
+        },
+      },
+      date: date,
+      attended: false,
     },
   });
 
